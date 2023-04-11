0a338a722bf75276ecbb6a7814b91605db87757f
Muuted <adam.s.knudsen@gmail.com>
2023-04-11 18:53:58 +0200

    setting everything up

---
+++ Classes.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Water_drops:
+
+    pass
+
+class Board:
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ Simulation_broad_in_water_falling.py
@@ -0,0 +1,29 @@
+from Classes import Water_drops,Board
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+
+clock = pygame.time.Clock()
+running = True
+
+
+while running:
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass

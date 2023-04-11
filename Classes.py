4f0cafd7134e2ca3eacac7e5f562c5c42cbb7c46
Muuted <adam.s.knudsen@gmail.com>
2023-04-11 20:41:45 +0200

    starting the classes

--- Classes.py
+++ Classes.py
@@ -1,10 +1,33 @@
 import pygame
 
-class Water_drops:
+
+class WaterDrops(pygame.sprite.Sprite):
+
+    def _init_(self, color, width, height, xv, yv, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.colors = color
+        self.width = width
+        self.height = height
+        self.xv = xv
+        self.yv = yv
+        self.x = x
+        self.y = y
+
 
     pass
 
-class Board:
+
+class Board(pygame.sprite.Sprite):
+    def _init_(self, color, width, height, xv, yv, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.colors = color
+        self.width = width
+        self.height = height
+        self.xv = xv
+        self.yv = yv
+        self.x = x
+        self.y = y
+
 
     pass
 
--- Simulation_broad_in_water_falling.py
+++ Simulation_broad_in_water_falling.py
@@ -7,12 +7,22 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+times = 0
 
 while running:
 
+
+    screen.fill("black")
+    pygame.display.flip()
+
+    clock.tick(60)
+    times += 1
+
+    if times > 100:
+        pygame.quit()
     pass
 
-
+pygame.quit()
 
 
 
